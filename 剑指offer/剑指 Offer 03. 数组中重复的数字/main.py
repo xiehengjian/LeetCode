7ac50cc72bef45e94e6345e71995eaa7caba48d3
xiehengjian <40929721+xiehengjian@users.noreply.github.com>
2020-06-21 09:11:53 +0800

    Update main.py

--- 剑指offer/剑指 Offer 03. 数组中重复的数字/main.py
+++ 剑指offer/剑指 Offer 03. 数组中重复的数字/main.py
@@ -1,1 +1,8 @@
-
+#暴力循环，但是最终超时了
+class Solution:
+    def findRepeatNumber(self, nums: List[int]) -> int:
+        while True:
+            num=nums[0]
+            del nums[0]
+            if num in nums:
+                return num
