a7c4ef91dee355e7908d2add7c68637b8ae8fb80
xiehengjian <40929721+xiehengjian@users.noreply.github.com>
2020-06-21 09:54:09 +0800

    Update main.go

--- 剑指offer/剑指 Offer 03. 数组中重复的数字/main.go
+++ 剑指offer/剑指 Offer 03. 数组中重复的数字/main.go
@@ -1,1 +1,11 @@
-
+//python中的暴力遍历搬过来的，但是Go没有in判断，所以又循环了一遍，速度比较慢
+func findRepeatNumber(nums []int) int {
+    for i:=0;i<len(nums);i++{
+        for j:=0;j<len(nums[:i]);j++{
+            if nums[i]==nums[:i][j]{
+                return nums[i]
+            }
+        }
+    }
+    return -1
+}
